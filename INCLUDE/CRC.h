6661d2fdab3c37df6c57dbf48e3dff2afd5746b7
August Tranberg <augusttranbergspam@gmail.com>
2025-10-08 15:37:35 +0200

    Simple CRC class to encode & decode errorhandling

---
+++ INCLUDE/CRC.h
@@ -0,0 +1,32 @@
+#ifndef CRC_H
+#define CRC_H
+
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+class CRC {
+
+    public:
+        CRC();
+        CRC(vector<int> denominator);
+
+        vector<vector<int>> split8(vector<int> binaryData);
+        vector<vector<int>> split12(vector<int> binaryData);
+
+        uint16_t vec2bin(vector<int> binaryData);
+        vector<int> bin2vec(uint16_t binaryData);
+
+        vector<uint16_t> encode1216(vector<uint16_t> splitBinaryData);
+        bool verify(uint16_t encodedBinaryData);
+        vector<uint16_t> decode1612(vector<uint16_t> encodedBinaryData);
+
+        ~CRC();
+
+    protected:
+        vector<int> denominator = {1, 0, 0, 1, 1};
+
+};
+
+#endif
---
+++ SRC/crctest.cpp
@@ -0,0 +1,46 @@
+#include "../INCLUDE/CRC.h"
+
+#include <iostream>
+#include <cstdint>
+
+
+int main() {
+
+    CRC crc;
+
+    vector<int> binaryData = {1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1};
+    // vector<int> binaryData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
+
+    vector<vector<int>> splitData = crc.split12(binaryData);
+
+    for (int i = 0; i < splitData.size(); i++) {
+        for (int j = 0; j < splitData[i].size(); j++) {
+            cout << splitData[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    vector<uint16_t> binData;
+
+    for(int i = 0; i < splitData.size(); i++) {
+        uint16_t temp = crc.vec2bin(splitData[i]);
+
+        binData.push_back(temp);
+    }
+
+    for(int i = 0; i < binData.size(); i++) {
+        cout << binData[i] << endl;
+    }
+
+    vector<uint16_t> encodedData = crc.encode1216(binData);
+
+    for(int i = 0; i < encodedData.size(); i++) {
+        cout << encodedData[i] << endl;
+    }
+
+    vector<uint16_t> decodedData = crc.decode1612(encodedData);
+
+    for(int i = 0; i < decodedData.size(); i++) {
+        cout << decodedData[i] << endl;
+    }
+}
