af0960f6c3755e1161dd4605df798e2d0eb5fcd4
FrederikWilkens <frwil24@student.sdu.dk>
2025-10-25 13:49:35 +0200

    Updated rb3

--- src/rb3_node_cpp.cpp
+++ src/rb3_node_cpp.cpp
@@ -1,69 +1,69 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include <chrono>      // For std::chrono::seconds
-#include <stdlib.h>    // For std::rand
+#include <chrono>
+#include <stdlib.h>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-
+#include "velocity_provider.hpp"   // <-- new
 using namespace std;
+using namespace std::chrono_literals; // for 1000ms
 
 class RB3_cpp_publisher : public rclcpp::Node{
   public:
-    //Create publisher for publishing veloctiy commands to topic "cmd_vel"
-    RB3_cpp_publisher()
-    : Node("rb3_cpp_publisher"){
+    // accept an injected provider (can be nullptr for fallback/random)
+    RB3_cpp_publisher(std::shared_ptr<VelocityProvider> provider = nullptr)
+    : Node("rb3_cpp_publisher"), provider_(provider) {
       RCLCPP_INFO(this->get_logger(), "Node started successfully!");
       publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("cmd_vel",10);
-
-      // Timer to periodically publish data
       timer_ = this->create_wall_timer(1000ms, std::bind(&RB3_cpp_publisher::publish_vel, this));
     }
 
   private:
-  //Class function to be called when you want to publish velocity commands
   void publish_vel(){
-    // Populate the header
-    msg.header.stamp = this->get_clock()->now(); // Set to current time		// CHECK om node og this kunne udskiftes
-    msg.header.frame_id = "base_link";           // Reference frame
+    msg.header.stamp = this->get_clock()->now();
+    msg.header.frame_id = "base_link";
 
-    // Populate the Twist part
-    // Set angular velocity to desired value (ie. turning)
-    msg.twist.angular.x = 0;
-    msg.twist.angular.y = 0;
-    msg.twist.angular.z = this->rand_FloatRange(0,2.84);
+    if (provider_) {
+      msg.twist.linear.x = provider_->getVel();
+      msg.twist.angular.z = provider_->getRot();
+    } else {
+      msg.twist.angular.x = 0;
+      msg.twist.angular.y = 0;
+      msg.twist.angular.z = this->rand_FloatRange(0,2.84);
 
-    // Set linear velocity to desired value (ie. forward and backwards)
-    msg.twist.linear.x = this->rand_FloatRange(0,0.22);
-    msg.twist.linear.y = 0;
-    msg.twist.linear.z = 0;
+      msg.twist.linear.x = this->rand_FloatRange(0,0.22);
+      msg.twist.linear.y = 0;
+      msg.twist.linear.z = 0;
+    }
+
     RCLCPP_INFO(this->get_logger(), "Publishing: %f , %f", this->msg.twist.linear.x, this->msg.twist.angular.z);
     publisher_->publish(msg);
-
   }
 
-  // Make random numner between a and b
   float rand_FloatRange(float a, float b)
   {
     return ((b - a) * ((float)rand() / RAND_MAX)) + a;
   }
 
-  // Private variables used for the publisher
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr publisher_;
   geometry_msgs::msg::TwistStamped msg;
+
+  std::shared_ptr<VelocityProvider> provider_;
 };
+// ...existing code...
 int main(int argc, char ** argv)
 {
-  // Initialize rclcpp
   rclcpp::init(argc, argv);
-  // "Spin" the node
-  rclcpp::spin(std::make_shared<RB3_cpp_publisher>());
-  // Shutdown node when complete
-  rclcpp::shutdown(); 
+
+  // create the provider from other file and inject it
+  auto provider = std::make_shared<OtherClass>(); // ensure OtherClass is visible here (include its header)
+  rclcpp::spin(std::make_shared<RB3_cpp_publisher>(provider));
+
+  rclcpp::shutdown();
   return 0;
-  
 }
