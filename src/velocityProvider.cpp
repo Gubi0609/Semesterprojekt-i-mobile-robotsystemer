6de2e740e8d8cc95092495a25fb1d835a0aa776e
AsmusRise <asris24@student.sdu.dk>
2025-11-05 09:07:51 +0100

    initial duration velocity code

--- src/velocityProvider.cpp
+++ src/velocityProvider.cpp
@@ -2,10 +2,23 @@
 #include <cstdlib>
 
 VelocityProvider::VelocityProvider()
-: linear_x_(0.0f), angular_z_(0.0f)
+: linear_x_(0.0f), angular_z_(0.0f), state_(State::IDLE), durationVelocity(0.0f)
 {}
 
 float VelocityProvider::getVel() {
+	if(state_ == State::DURATION){
+		if(std::chrono::steady_clock::now() <end_time_){
+			//endtime is greater that timenow, which means not enough time has passed
+			//endtime is evaluated at driveForDuration
+			return durationVelocity;
+		}
+		else{ //once the duration has passed, the mode i set to idle
+			state_ = State::IDLE;
+			durationVelocity = 0.0f; //maybe setting linearx and returning it is abundant. 
+			linear_x_ = 0.0f;
+			return linear_x_;
+		}
+	}
 	return linear_x_;
 }
 
@@ -14,6 +27,9 @@ float VelocityProvider::getRot(){
 }
 
 void VelocityProvider::setVel(float f){
+	if(f>0.22) f=0.22;
+	if(f<0) f =0.0f;
+
 	linear_x_ = f;
 }
 
@@ -21,4 +37,9 @@ void VelocityProvider::setRot(float f){
 	angular_z_ = f;
 }
 
+void VelocityProvider::driveForDuration(int duration, float velocity){
+	durationVelocity = velocity;
+	end_time_ = std::chrono::steady_clock::now() + std::chrono::seconds(duration);
+	state_ = State::DURATION;
+}
 
--- src/velocityProvider.hpp
+++ src/velocityProvider.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdlib.h>    // For std::rand
+#include <chrono>
+
 
 class VelocityProvider {
 public:
@@ -10,7 +12,19 @@ public:
     float getRot(); //return angular z
     void setVel(float f);
     void setRot(float f);
+    void driveForDuration(int d, float v); //takes duration as input parameter.
+
+    enum class State {IDLE, DURATION};
+    State state() const {return state_;}
+
 private:
     float linear_x_;
     float angular_z_;
+    time_t time_stamp;
+    bool timeStampSet = false;
+
+    //State data
+    State state_ = State::IDLE;
+    float durationVelocity = 0.0f;
+    std::chrono::steady_clock::time_point end_time_;
 };
